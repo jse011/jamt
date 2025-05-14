ee462a61909bf6732cc3a3514b0563f1705598ed
jsefao <55448613+jse011@users.noreply.github.com>
2025-05-14 16:30:32 -0500

    checkIn an CheckOut

--- lib/feature/qr/view/qr_screen.dart
+++ lib/feature/qr/view/qr_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jamt/feature/qr/bloc/qr_bloc.dart';
